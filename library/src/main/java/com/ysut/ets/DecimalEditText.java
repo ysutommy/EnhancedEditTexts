6bbdca6b46af581001e9d4e912832264cdb6e053
ysutommy <tang_miao@sina.cn>
2017-12-13 13:37:31 +0800

    release 1.0

---
+++ library/src/main/java/com/ysut/ets/DecimalEditText.java
@@ -0,0 +1,94 @@
+package com.ysut.ets;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * 自定义保持小数位数的编辑框
+ *
+ * <p/>
+ * Created by gaopoy on 2016/10/19.
+ */
+public class DecimalEditText extends EditText {
+
+    private int digits = 2; //小数位数，默认2位
+    private float maxValue = Float.MAX_VALUE;
+
+    public DecimalEditText(Context context) {
+        this(context, null);
+    }
+
+    public DecimalEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public DecimalEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @TargetApi(21)
+    public DecimalEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    @Override
+    public void addTextChangedListener(TextWatcher watcher) {
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.DecimalEditText);
+            try {
+                digits = ta.getInt(R.styleable.DecimalEditText_decimal_digits, digits);
+                maxValue = ta.getFloat(R.styleable.DecimalEditText_decimal_max_value, maxValue);
+            } finally {
+                ta.recycle();
+            }
+        }
+        setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        super.addTextChangedListener(new InnerTextWatcher());
+    }
+
+    class InnerTextWatcher implements TextWatcher {
+        String temp;
+        int dot;
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (s.length() == 0) return;
+            temp = s.toString();
+            dot = s.charAt(0);
+            if (dot == '-') {
+                s.delete(0, 1);
+            } else if (dot == '.') {
+                s.insert(0, "0");
+            } else if (dot == '0' && s.length() == 2 && s.charAt(1) != '.') {
+                s.insert(1, ".");
+            } else if (Double.parseDouble(temp) > maxValue) {
+                s.delete(s.length() - 1, s.length());
+            } else if (!temp.endsWith(".")) {
+                int dot = temp.indexOf('.');
+                if (dot > 0 && temp.substring(dot + 1, s.length()).length() > digits) {
+                    s.delete(s.length() - 1, s.length());
+                }
+            }
+        }
+    }
+}
---
+++ library/src/main/java/com/ysut/ets/SpilttedEditText.java
@@ -0,0 +1,113 @@
+package com.ysut.ets;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.text.method.DigitsKeyListener;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ *
+ * Created by gaopoy on 2016/10/18.
+ * */
+public class SpilttedEditText extends EditText {
+
+    private TextWatcher textWatcher;
+    private String separator = " ";
+    private int sepLen = 4;
+    private String defAllowedChars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ -,";
+
+    public SpilttedEditText(Context context) {
+        this(context, null);
+    }
+
+    public SpilttedEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public SpilttedEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @TargetApi(21)
+    public SpilttedEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.SpilttedEditText);
+            try {
+                String temp = ta.getString(R.styleable.SpilttedEditText_separator);
+                if (!TextUtils.isEmpty(temp)) separator = temp;
+                temp = ta.getString(R.styleable.SpilttedEditText_allowed_input);
+                if (!TextUtils.isEmpty(temp)) defAllowedChars = temp;
+                sepLen = ta.getInt(R.styleable.SpilttedEditText_sep_len, sepLen);
+            } finally {
+                ta.recycle();
+            }
+        }
+        if (sepLen > 0) {
+            if (!TextUtils.isEmpty(defAllowedChars))setKeyListener(DigitsKeyListener.getInstance(defAllowedChars));
+            textWatcher = new InnerTextWatcher();
+            super.addTextChangedListener(textWatcher);
+        }
+    }
+
+
+    @Override
+    public void addTextChangedListener(TextWatcher watcher) {
+        if (textWatcher != null) removeTextChangedListener(textWatcher);
+        super.addTextChangedListener(watcher);
+    }
+
+    class InnerTextWatcher implements TextWatcher {
+        final StringBuffer buffer = new StringBuffer();
+        int len, start, selection;
+
+        void reInitBuffer() {
+            len = buffer.length();
+            start = sepLen;
+            while (start < len++) {
+                buffer.insert(start, separator);
+                start += sepLen + 1;
+            }
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            int len = buffer.length();
+            selection = getSelectionEnd();
+            buffer.delete(0, len);
+            buffer.append(s.toString().replace(separator, ""));
+            reInitBuffer();
+            removeTextChangedListener(textWatcher);
+
+            setText(buffer.toString());
+
+            if (buffer.length() > len) {
+                setSelection(buffer.length());
+            } else {
+                setSelection(selection > buffer.length() ? buffer.length() : selection);
+            }
+
+            SpilttedEditText.super.addTextChangedListener(textWatcher);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    }
+
+}
